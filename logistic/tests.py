555b5c2ac1c21bfd791265a7f5107e342fa11302
Sergey Kievskiy <knhel7@mail.ru>
2023-08-23 18:14:39 +0300

    Add CI_CD pipeline

---
+++ logistic/tests.py
@@ -0,0 +1,10 @@
+from rest_framework.test import APIClient
+from unittest import TestCase
+
+
+class TestAPIView(TestCase):
+    def test_Hi(self):
+        client = APIClient()
+        response = client.get('/api/v1/')
+        self.assertEqual(response.data, 'Hi, people!')
+        # self.assertEqual(True, False)  # add assertion here
