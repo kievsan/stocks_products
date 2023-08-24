1af7c02c64e27d71ec8af2dc83cfb188e0db343a
Sergey Kievskiy <knhel7@mail.ru>
2023-08-24 21:20:51 +0300

    CI_CD, corrects-3

--- logistic/tests.py
+++ logistic/tests.py
@@ -6,5 +6,5 @@ class TestAPIView(TestCase):
     def test_Hi(self):
         client = APIClient()
         response = client.get('/api/v1/')
-        self.assertEqual(response.data, 'Hi, people!')
+        self.assertEqual(response.data, 'Hi, people!!!')
         # self.assertEqual(True, False)  # add assertion here
